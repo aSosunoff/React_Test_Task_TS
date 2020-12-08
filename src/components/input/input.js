d73b63e08d30b2adef068427fd0cea833a9568cc
asosunoff <al.sosunoff@gmail.com>
2020-12-08 19:08:22 +0300

    Added Input component

---
+++ src/components/input/input.js
@@ -0,0 +1,86 @@
+import React, { useCallback, useMemo } from "react";
+import PropTypes from "prop-types";
+import { v4 } from "uuid";
+import cn from "classnames";
+
+const Input = ({
+	type,
+	label,
+	invalid,
+	invalidMessage,
+	className,
+	styles,
+	onChange,
+	...attrsInput
+}) => {
+	const uniq = useMemo(() => v4(), []);
+
+	const onChangeHandler = useCallback(({ target }) => onChange(target.value), [
+		onChange,
+	]);
+	return (
+		<div>
+			<input
+				id={uniq}
+				type={type}
+				className={cn({ invalid, className })}
+				onChange={onChangeHandler}
+				{...attrsInput}
+			/>
+
+			<label
+				htmlFor={uniq}
+				className={cn({
+					active: Boolean(attrsInput?.value),
+				})}
+			>
+				{label}
+			</label>
+
+			{invalid ? (
+				<small className="helper-text invalid" v-if="invalid">
+					{invalidMessage}
+				</small>
+			) : null}
+		</div>
+	);
+};
+
+Input.defaultProps = {
+	invalidMessage: "Необходимо заполнить поле",
+	type: "text",
+};
+
+Input.propTypes = {
+	label: PropTypes.string,
+	invalidMessage: PropTypes.string,
+	invalid: PropTypes.bool,
+	disabled: PropTypes.bool,
+	type: PropTypes.oneOf([
+		"button",
+		"checkbox",
+		"file",
+		"hidden",
+		"image",
+		"password",
+		"radio",
+		"reset",
+		"submit",
+		"text",
+		"color",
+		"date",
+		"datetime",
+		"datetime-local",
+		"email",
+		"number",
+		"range",
+		"search",
+		"tel",
+		"time",
+		"url",
+		"month",
+		"week",
+	]),
+};
+
+export default Input;
