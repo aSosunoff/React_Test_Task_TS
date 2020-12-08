2b458871d953541c12b337fd577e16be774146bd
asosunoff <al.sosunoff@gmail.com>
2020-12-08 23:36:22 +0300

    Layout fix

--- src/components/layout/emptyLayout/EmptyLayout.js
+++ src/components/layout/emptyLayout/EmptyLayout.js
@@ -1,9 +1,18 @@
 import React from "react";
 import { Route } from "react-router-dom";
+import styles from "./EmptyLayout.module.scss";
+import cn from "classnames";
 
 const EmptyLayout = ({ children, ...props }) => {
 	return (
-		<div className="container">
+		<div
+			className={cn(
+				styles["grid-layout"],
+				styles["grid-layout_center"],
+				"blue-grey",
+				"darken-3"
+			)}
+		>
 			<Route {...props}>{children}</Route>
 		</div>
 	);
