5662bce312fc8ec43316cde6e8dbc4695693b57b
asosunoff <al.sosunoff@gmail.com>
2020-12-08 19:54:07 +0300

    Added base button

---
+++ src/components/button/baseButton/BaseButton.js
@@ -0,0 +1,59 @@
+import React from "react";
+import PropTypes from "prop-types";
+import cn from "classnames";
+import styles from "./BaseButton.module.scss";
+
+const BaseButton = ({
+	tag,
+	type,
+	disabled,
+	invalid,
+	invalidMessage,
+	children,
+	...props
+}) => {
+	const classDisabled = { [styles.disabled]: disabled };
+
+	if (disabled) {
+		delete props.onClick;
+	}
+
+	const buttonElement = React.createElement(
+		tag,
+		{
+			key: "button",
+			className: cn(styles.button, classDisabled),
+			type,
+			...props,
+		},
+		children
+	);
+
+	const invalidElement = invalid
+		? React.createElement(
+				"small",
+				{
+					key: "error",
+					className: cn(styles.button__invalid, classDisabled),
+				},
+				invalidMessage
+		  )
+		: null;
+
+	return React.createElement("div", null, [buttonElement, invalidElement]);
+};
+
+BaseButton.defaultProps = {
+	tag: "button",
+	type: "button",
+};
+
+BaseButton.propTypes = {
+	tag: PropTypes.oneOf(["button", "a", "Link"]),
+	type: PropTypes.oneOf(["button", "submit", "reset"]),
+	disabled: PropTypes.bool,
+	invalid: PropTypes.bool,
+	invalidMessage: PropTypes.string,
+};
+
+export default BaseButton;
