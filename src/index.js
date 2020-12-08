0d00d8bc5138dcf482c0b9b47e983c9a191b41f1
asosunoff <al.sosunoff@gmail.com>
2020-12-08 19:54:15 +0300

    Added variable

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/app";
+import './variable.scss';
 import "materialize-css/dist/js/materialize.min.js";
 import "materialize-css/dist/css/materialize.min.css";
 
