7a93a9bf32c60538bc7646ce26260f7224ef0a34
asosunoff <al.sosunoff@gmail.com>
2021-02-11 22:40:16 +0300

    update useForm

--- src/components/modalContact/ModalContact.js
+++ src/components/modalContact/ModalContact.js
@@ -1,7 +1,7 @@
 import React, { useCallback, useEffect } from "react";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
-
+import is from "is_js";
 import { useForm } from "@asosunoff/react_use_form";
 import { editContacts, addContacts } from "../../redux/actions";
 import { contactSelector } from "../../redux/selectors/contactsSelectors";
@@ -11,102 +11,99 @@ import Input from "../UI/input";
 import { info } from "../../utils/toast";
 
 const INITIAL_VALUES = {
-	id: {
-		value: null,
-	},
-	name: {
-		value: "",
-		validation: {
-			required: {
-				message: "Необходимо заполнить поле",
-			},
-		},
-	},
-	phone: {
-		value: "",
-		validation: {
-			required: {
-				message: "Необходимо заполнить поле",
-			},
-		},
-	},
+  id: {
+    value: null,
+  },
+  name: {
+    value: "",
+    validation: (value) => {
+      if (is.empty(value)) {
+        return {
+          errorMessage: "Необходимо заполнить поле",
+        };
+      }
+    },
+  },
+  phone: {
+    value: "",
+    validation: (value) => {
+      if (is.empty(value)) {
+        return {
+          errorMessage: "Необходимо заполнить поле",
+        };
+      }
+    },
+  },
 };
 
-function ModalContact({
-	contact,
-	editContacts,
-	addContacts,
-	isShow,
-	onHideModal,
-}) {
-	const { values, handlers, setValue, reset, isFormInvalid } = useForm(
-		INITIAL_VALUES
-	);
+function ModalContact({ contact, editContacts, addContacts, isShow, onHideModal }) {
+  const { values, handlers, setValue, resetHandler, isInvalidForm } = useForm(INITIAL_VALUES);
 
-	useEffect(() => {
-		if (isShow) {
-			if (contact?.id) {
-				setValue(contact);
-			} else {
-				reset();
-			}
-		}
-	}, [setValue, reset, isShow, contact]);
+  useEffect(() => {
+    if (isShow) {
+      if (contact?.id) {
+        setValue(contact);
+      } else {
+        resetHandler();
+      }
+    }
+  }, [setValue, resetHandler, isShow, contact]);
 
-	const submitEditHandler = useCallback(() => {
-		editContacts(values);
-		info("Контакт изменён");
-		onHideModal();
-	}, [values, editContacts, onHideModal]);
+  const submitEditHandler = useCallback(() => {
+    editContacts(values);
+    info("Контакт изменён");
+    onHideModal();
+  }, [values, editContacts, onHideModal]);
 
-	const submitAddHandler = useCallback(() => {
-		addContacts(values);
-		info("Контакт добавлен");
-		onHideModal();
-	}, [values, addContacts, onHideModal]);
+  const submitAddHandler = useCallback(() => {
+    addContacts(values);
+    info("Контакт добавлен");
+    onHideModal();
+  }, [values, addContacts, onHideModal]);
 
-	return (
-		<Modal isShow={isShow} onHideModal={onHideModal}>
-			<Modal.Title>
-				{contact?.id ? "Редактирование" : "Добавление"} контактов
-			</Modal.Title>
+  return (
+    <Modal isShow={isShow} onHideModal={onHideModal}>
+      <Modal.Title>{contact?.id ? "Редактирование" : "Добавление"} контактов</Modal.Title>
 
-			<Modal.Body>
-				<Input
-					label="Имя"
-					value={handlers.name.value}
-					onChange={handlers.name.onChange}
-					invalid={handlers.name.touched && handlers.name.invalid}
-					invalidMessage={handlers.name.invalidMessage}
-				/>
+      <Modal.Body>
+        <Input
+          label="Имя"
+          value={handlers.name.value}
+          onChange={handlers.name.onChange}
+          invalid={handlers.name.touched && handlers.name.invalid}
+          invalidMessage={handlers.name.invalidMessage}
+        />
 
-				<Input
-					label="Телефон"
-					value={handlers.phone.value}
-					onChange={handlers.phone.onChange}
-					invalid={handlers.phone.touched && handlers.phone.invalid}
-					invalidMessage={handlers.phone.invalidMessage}
-				/>
-			</Modal.Body>
+        <Input
+          label="Телефон"
+          value={handlers.phone.value}
+          onChange={handlers.phone.onChange}
+          invalid={handlers.phone.touched && handlers.phone.invalid}
+          invalidMessage={handlers.phone.invalidMessage}
+        />
+      </Modal.Body>
 
-			<Modal.Footer>
-				{contact?.id ? (
-					<BaseButton disabled={isFormInvalid} onClick={submitEditHandler}>
-						Сохранить
-					</BaseButton>
-				) : (
-					<BaseButton disabled={isFormInvalid} onClick={submitAddHandler}>
-						Сохранить
-					</BaseButton>
-				)}
-			</Modal.Footer>
-		</Modal>
-	);
+      <Modal.Footer>
+        {contact?.id ? (
+          <BaseButton disabled={isInvalidForm} onClick={submitEditHandler}>
+            Сохранить
+          </BaseButton>
+        ) : (
+          <BaseButton disabled={isInvalidForm} onClick={submitAddHandler}>
+            Сохранить
+          </BaseButton>
+        )}
+        <BaseButton disabled={isInvalidForm} onClick={resetHandler}>
+          Сбросить
+        </BaseButton>
+      </Modal.Footer>
+    </Modal>
+  );
 }
 
 export default connect(
-	createStructuredSelector({
-		contact: contactSelector,
-	}),
-	{ editContacts, addContacts }
+  createStructuredSelector({
+    contact: contactSelector,
+  }),
+  { editContacts, addContacts }
 )(ModalContact);
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,4 +1,5 @@
 import React, { useCallback, useEffect } from "react";
+import is from "is_js";
 import { useHistory } from "react-router-dom";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
@@ -12,103 +13,109 @@ import { login } from "../redux/actions";
 import { authUserSelectors } from "../redux/selectors";
 
 const INITIAL_VALUES = {
-	login: {
-		value: "",
-		validation: {
-			required: {
-				message: "Необходимо заполнить поле",
-			},
-		},
-	},
-	password: {
-		value: "",
-		validation: {
-			required: {
-				message: "Необходимо заполнить поле",
-			},
-			minLength: {
-				value: 4,
-				message: "Длина должна быть не меньше 4 символов",
-			},
-		},
-	},
+  login: {
+    value: "",
+    validation: (value) => {
+      if (is.empty(value)) {
+        return {
+          errorMessage: "Необходимо заполнить поле",
+        };
+      }
+    },
+  },
+  password: {
+    value: "",
+    validation: (value) => {
+      if (is.empty(value)) {
+        return {
+          errorMessage: "Необходимо заполнить поле",
+        };
+      }
+
+      if (`${value}`.length < 4) {
+        return {
+          errorMessage: "Длина должна быть не меньше 4 символов",
+        };
+      }
+    },
+  },
 };
 
 const Login = ({ isAuthenticated, login, loading, loaded, error }) => {
-	const history = useHistory();
+  const history = useHistory();
 
-	useEffect(() => {
-		if (!loading && loaded && isAuthenticated) {
-			history.push("/");
-		} else if (!loading && loaded && !isAuthenticated) {
-			warning("Введены не верные данные");
-		} else if (error) {
-			danger(error.message);
-		}
-	}, [history, isAuthenticated, loading, error, loaded]);
+  useEffect(() => {
+    if (!loading && loaded && isAuthenticated) {
+      history.push("/");
+    } else if (!loading && loaded && !isAuthenticated) {
+      warning("Введены не верные данные");
+    } else if (error) {
+      danger(error.message);
+    }
+  }, [history, isAuthenticated, loading, error, loaded]);
 
-	const { values, handlers, isFormInvalid } = useForm(INITIAL_VALUES);
+  const { values, handlers, isFormInvalid } = useForm(INITIAL_VALUES);
 
-	const loginHandler = useCallback(
-		(e) => {
-			e.preventDefault();
-			login(values);
-		},
-		[login, values]
-	);
+  const loginHandler = useCallback(
+    (e) => {
+      e.preventDefault();
+      login(values);
+    },
+    [login, values]
+  );
 
-	return (
-		<form
-			className="card hoverable"
-			style={{
-				gridArea: "cc",
-				margin: 0,
-			}}
-			onSubmit={loginHandler}
-		>
-			<div className="card-content">
-				<span className="card-title">Вход в систему</span>
+  return (
+    <form
+      className="card hoverable"
+      style={{
+        gridArea: "cc",
+        margin: 0,
+      }}
+      onSubmit={loginHandler}
+    >
+      <div className="card-content">
+        <span className="card-title">Вход в систему</span>
 
-				<Input
-					label="Логин"
-					disabled={loading}
-					value={handlers.login.value}
-					onChange={handlers.login.onChange}
-					invalid={handlers.login.touched && handlers.login.invalid}
-					invalidMessage={handlers.login.invalidMessage}
-				/>
+        <Input
+          label="Логин"
+          disabled={loading}
+          value={handlers.login.value}
+          onChange={handlers.login.onChange}
+          invalid={handlers.login.touched && handlers.login.invalid}
+          invalidMessage={handlers.login.invalidMessage}
+        />
 
-				<Input
-					label="Пароль"
-					type="password"
-					disabled={loading}
-					value={handlers.password.value}
-					onChange={handlers.password.onChange}
-					invalid={handlers.password.touched && handlers.password.invalid}
-					invalidMessage={handlers.password.invalidMessage}
-				/>
-			</div>
+        <Input
+          label="Пароль"
+          type="password"
+          disabled={loading}
+          value={handlers.password.value}
+          onChange={handlers.password.onChange}
+          invalid={handlers.password.touched && handlers.password.invalid}
+          invalidMessage={handlers.password.invalidMessage}
+        />
+      </div>
 
-			<div className="card-action">
-				<BlackButton type="submit" disabled={isFormInvalid || loading}>
-					Войти
-					<i className="material-icons right">send</i>
-				</BlackButton>
-			</div>
+      <div className="card-action">
+        <BlackButton type="submit" disabled={isFormInvalid || loading}>
+          Войти
+          <i className="material-icons right">send</i>
+        </BlackButton>
+      </div>
 
-			<Progress canVisible={loading} />
-		</form>
-	);
+      <Progress canVisible={loading} />
+    </form>
+  );
 };
 
 export default connect(
-	createStructuredSelector({
-		isAuthenticated: authUserSelectors.isAuthenticatedSelector,
-		loading: authUserSelectors.loginLoadingSelector,
-		loaded: authUserSelectors.loginLoadedSelector,
-		error: authUserSelectors.loginErrorSelector,
-	}),
-	{
-		login,
-	}
+  createStructuredSelector({
+    isAuthenticated: authUserSelectors.isAuthenticatedSelector,
+    loading: authUserSelectors.loginLoadingSelector,
+    loaded: authUserSelectors.loginLoadedSelector,
+    error: authUserSelectors.loginErrorSelector,
+  }),
+  {
+    login,
+  }
 )(Login);
